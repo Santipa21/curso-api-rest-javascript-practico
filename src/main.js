5e91e538b29faf2d4a269689c366b8d67c2ed14f
Santi Patiño <santiagopatino@flare.com.co>
2024-10-10 17:46:55 -0500

    Error: carga duplicada de datos

--- src/main.js
+++ src/main.js
@@ -11,9 +11,9 @@ async function getTrendingMoviesPreview() {
     const { data } = await api('trending/movie/day');
 
     const movies = data.results;
-    
+    trendingMoviesPreviewList.innerHTML = "";
     movies.forEach(movie => {
-        const trendingMoviesPreviewList =  document.querySelector('#trendingPreview .trendingPreview-movieList')
+       
         const movieContainer =  document.createElement('div')
         movieContainer.classList.add('movie-container');
 
@@ -31,8 +31,9 @@ async function getCategoriesPreview() {
     const { data } = await api('genre/movie/list');
 
     const categories = data.genres;
+
+    categoriesPreviewList.innerHTML = "";
     categories.forEach(category => {
-        const categoriesPreviewList =  document.querySelector('#categoriesPreview .categoriesPreview-list')
 
         const categoryContainer =  document.createElement('div')
         categoryContainer.classList.add('category-container');
